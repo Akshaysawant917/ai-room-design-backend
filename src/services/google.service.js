901861e4cc2f2fcc79796f7a27294579a458bf10
Akshay Sawant <akshaysawant917@gmail.com>
2026-09-08 15:24:19 +0530

    callack  update

--- src/services/google.service.js
+++ src/services/google.service.js
@@ -10,7 +10,7 @@ export function configureGoogle() {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/api/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://design.getminidesk.com/api/auth/google/callback'
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       const account = profile._json;
